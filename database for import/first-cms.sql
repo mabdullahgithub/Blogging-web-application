12d6efd3e35bd6ddc924433248765150a94942db
Muhammad Abdullah <abdullahorg15@gmail.com>
2023-09-20 02:09:27 -0700

    replacement of database file

--- database for import/first-cms.sql
+++ database for import/first-cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2023 at 09:02 AM
+-- Generation Time: Sep 20, 2023 at 11:08 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -66,8 +66,8 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`category_id`, `category_name`, `post`) VALUES
 (1, 'funny', 1),
-(2, ' entertainment', 0),
-(3, 'superior', 1),
+(2, ' entertainment', 1),
+(3, 'superior', 2),
 (4, 'sports', 0),
 (5, 'With News', 1),
 (6, 'Universities', 1),
@@ -99,7 +99,9 @@ INSERT INTO `post` (`post_id`, `title`, `description`, `date`, `category`, `auth
 (11, 'Umair Blogging', 'Fusce quis ipsum pellentesque, suscipit sapien eget, elementum nisi. Cras mattis libero at enim vestibulum aliquet. Pellentesque pulvinar, eros ut dapibus efficitur, nulla turpis rhoncus urna, eget lobortis urna purus placerat velit. Vestibulum a hendrerit diam. Praesent volutpat orci sit amet leo consectetur mollis. Suspendisse euismod ipsum sit amet faucibus convallis. Fusce fringilla diam lacus, sed accumsan diam tristique a. Donec mollis vulputate enim, ac tempus ex lacinia sed. Maecenas sit amet ipsum eu risus malesuada congue. Ut dapibus, leo eget porta dapibus, metus enim condimentum turpis, a mollis orci sapien sed orci. Nulla pulvinar neque ut enim sodales imperdiet.\r\n\r\nCurabitur nec tortor auctor magna interdum efficitur ac et libero. In cursus nibh lorem. Ut quis justo et urna bibendum facilisis eget ac est. Quisque sed tincidunt lectus. Aenean ex lectus, pulvinar id sagittis sit amet, varius iaculis ex. Fusce sit amet turpis eget arcu convallis dapibus. Aliquam vulputate posuere dolor vitae scelerisque. Nullam pulvinar augue sed pharetra rhoncus. Fusce molestie, justo vitae molestie lacinia, nunc urna porta mi, ullamcorper ullamcorper nulla eros ut massa. Donec varius orci in diam aliquam ullamcorper. Proin nec lobortis nisl. Cras eleifend sit amet ex et pretium.', '0000-00-00', 5, 3, '1695155299-pexels-irina-iriser-1379636.jpg'),
 (12, 'Project Completion', 'Nullam euismod sagittis mauris, a vestibulum elit hendrerit id. Proin eget eros urna. Suspendisse potenti. Suspendisse eu ante varius, pretium erat vel, ullamcorper risus. Cras scelerisque tristique erat nec ultrices. Curabitur non eros enim. Mauris pellentesque viverra enim, non auctor lorem interdum sit amet. Ut venenatis dapibus lacus, id fermentum dolor mollis in. Ut non ligula nisi. Pellentesque aliquam molestie vehicula. Vivamus maximus nulla enim, quis condimentum lectus pulvinar non. Donec tristique euismod tristique. Mauris auctor scelerisque lacus, sit amet lacinia felis tincidunt id. Integer rutrum ex at diam facilisis, nec convallis arcu rutrum. Sed sit amet pretium enim. Vestibulum hendrerit ornare ligula.', '0000-00-00', 6, 3, '1695155352-pexels-pok-rie-2049422.jpg'),
 (13, 'AMazing things', 'Nullam euismod sagittis mauris, a vestibulum elit hendrerit id. Proin eget eros urna. Suspendisse potenti. Suspendisse eu ante varius, pretium erat vel, ullamcorper risus. Cras scelerisque tristique erat nec ultrices. Curabitur non eros enim. Mauris pellentesque viverra enim, non auctor lorem interdum sit amet. Ut venenatis dapibus lacus, id fermentum dolor mollis in. Ut non ligula nisi. Pellentesque aliquam molestie vehicula. Vivamus maximus nulla enim, quis condimentum lectus pulvinar non. Donec tristique euismod tristique. Mauris auctor scelerisque lacus, sit amet lacinia felis tincidunt id. Integer rutrum ex at diam facilisis, nec convallis arcu rutrum. Sed sit amet pretium enim. Vestibulum hendrerit ornare ligula.', '0000-00-00', 8, 3, '1695155379-pexels-ryan-west-1719648.jpg'),
-(14, 'Addtion for date', 'Nullam euismod sagittis mauris, a vestibulum elit hendrerit id. Proin eget eros urna. Suspendisse potenti. Suspendisse eu ante varius, pretium erat vel, ullamcorper risus. Cras scelerisque tristique erat nec ultrices. Curabitur non eros enim. Mauris pellentesque viverra enim, non auctor lorem interdum sit amet. Ut venenatis dapibus lacus, id fermentum dolor mollis in. Ut non ligula nisi. Pellentesque aliquam molestie vehicula. Vivamus maximus nulla enim, quis condimentum lectus pulvinar non. Donec tristique euismod tristique. Mauris auctor scelerisque lacus, sit amet lacinia felis tincidunt id. Integer rutrum ex at diam facilisis, nec convallis arcu rutrum. Sed sit amet pretium enim. Vestibulum hendrerit ornare ligula.', '19 Sep, 2023', 1, 3, '1695156859-pexels-erik-mclean-9846087.jpg');
+(14, 'Addtion for date', 'Nullam euismod sagittis mauris, a vestibulum elit hendrerit id. Proin eget eros urna. Suspendisse potenti. Suspendisse eu ante varius, pretium erat vel, ullamcorper risus. Cras scelerisque tristique erat nec ultrices. Curabitur non eros enim. Mauris pellentesque viverra enim, non auctor lorem interdum sit amet. Ut venenatis dapibus lacus, id fermentum dolor mollis in. Ut non ligula nisi. Pellentesque aliquam molestie vehicula. Vivamus maximus nulla enim, quis condimentum lectus pulvinar non. Donec tristique euismod tristique. Mauris auctor scelerisque lacus, sit amet lacinia felis tincidunt id. Integer rutrum ex at diam facilisis, nec convallis arcu rutrum. Sed sit amet pretium enim. Vestibulum hendrerit ornare ligula.', '19 Sep, 2023', 1, 3, '1695156859-pexels-erik-mclean-9846087.jpg'),
+(17, 'testing post', 'centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n\r\nWhy do we use it?\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now', '20 Sep, 2023', 2, 3, '1695195957-pexels-irina-iriser-1379636.jpg'),
+(18, 'ABuzar ', 'centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n\r\nWhy do we use it?\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now', '20 Sep, 2023', 3, 5, '1695199440-Blog_single@2x.jpg');
 
 -- --------------------------------------------------------
 
@@ -119,8 +121,8 @@ CREATE TABLE `setting` (
 --
 
 INSERT INTO `setting` (`setting_id`, `websitename`, `logo`, `footerdesc`) VALUES
-(1, 'iprimetimes', '1695154956-Logo_2.svg', '@2023 designed by iprimetimes a blogging Company\r\nAll Right Reserved.'),
-(2, 'iprimetimes', '1695154956-Logo_2.svg', '@2023 designed by iprimetimes a blogging Company\r\nAll Right Reserved.');
+(1, 'iprimetimes', '1695196845-Logo_2.svg', '@2023 designed by iprimetimes a blogging Company\r\nAll Right Reserved.'),
+(2, 'iprimetimes', '1695196845-Logo_2.svg', '@2023 designed by iprimetimes a blogging Company\r\nAll Right Reserved.');
 
 -- --------------------------------------------------------
 
@@ -144,7 +146,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `first-name`, `last-name`, `username`, `email`, `password`, `role`) VALUES
 (3, 'Muhammad', 'Abdullah', 'abdullah', 'aaaa20112003@gmail.com', '93ec71b22793a81569c94ca17e4d9c293d8e201f', 1),
-(4, 'umiar', 'tufail', 'umair', 'umair@gmail.com', 'c9b9c416cb908bff0c82dd9bec5372d0d4b21607', 0);
+(4, 'umiar', 'tufail', 'umair', 'umair@gmail.com', 'c9b9c416cb908bff0c82dd9bec5372d0d4b21607', 0),
+(5, 'Abuzar', 'Talha', 'talha', 'talha@gmail.com', '93ec71b22793a81569c94ca17e4d9c293d8e201f', 0);
 
 --
 -- Indexes for dumped tables
@@ -200,7 +203,7 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `setting`
@@ -212,7 +215,7 @@ ALTER TABLE `setting`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
