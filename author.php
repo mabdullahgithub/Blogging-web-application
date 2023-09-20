b00a6ff565a906265238754d63c481fe8c28f11f
Muhammad Abdullah <abdullahorg15@gmail.com>
2023-09-20 02:07:54 -0700

    some changes related to search
    search.php replaced
    and some pagination setting

--- author.php
+++ author.php
@@ -107,7 +107,7 @@
         <button type="button" class="close" aria-label="Close" data-dismiss="searchModal">
             <span aria-hidden="true">&times;</span>
         </button>
-        <form action="index.html" method="get" class="oleez-overlay-search-form">
+        <form action="search.php" method="get" class="oleez-overlay-search-form">
             <label for="search" class="sr-only">Search</label>
             <input type="search" class="oleez-overlay-search-input" id="search" name="search" placeholder="Add words and hit enter to search">
         </form>
